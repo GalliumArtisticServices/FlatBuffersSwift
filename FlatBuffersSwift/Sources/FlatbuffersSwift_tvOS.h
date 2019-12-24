8aed98ded4b1e80ce4ab8607f0ce2b574eb2334a
Timothy Johnson <timothy.johnson@teamproxi.com>
2019-12-24 10:37:42 -0800

    Added support for iOS and tvOS, some code reorg

---
+++ FlatBuffersSwift/Sources/FlatbuffersSwift_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  FlatbuffersSwift_tvOS.h
+//  FlatbuffersSwift-tvOS
+//
+//  Created by Timothy Johnson on 12/24/19.
+//  Copyright © 2019 maxim.zaks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlatbuffersSwift_tvOS.
+FOUNDATION_EXPORT double FlatbuffersSwift_tvOSVersionNumber;
+
+//! Project version string for FlatbuffersSwift_tvOS.
+FOUNDATION_EXPORT const unsigned char FlatbuffersSwift_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlatbuffersSwift_tvOS/PublicHeader.h>
+
+
